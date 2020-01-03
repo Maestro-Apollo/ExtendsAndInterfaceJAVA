84481bbd2b55f5060aea5401702e2a13aa743de6
TheElric <rafimahafid@gmail.com>
2020-01-03 22:23:38 +0600

    Initial commit.

---
+++ src/shape/Circle.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shape;
+
+/**
+ *
+ * @author Rafi
+ */
+public class Circle extends Trapi implements Basic,Triangle{
+        public void getInfo(){
+            System.out.println("Hello Info");
+        
+        }
+        @Override
+        public void getLocation(){
+            super.getLocation();
+        }
+    public void getName(){
+    
+    
+    }
+    public void getAddress(){
+        
+    }
+    public void getVoice(){
+        
+    }
+    
+}
